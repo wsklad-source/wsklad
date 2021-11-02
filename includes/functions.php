db3401a821deef6a7e1bd5539a9de36c87dd004e
digiom-ceo <ceo@digiom.ru>
2021-11-02 21:21:27 +0300

    Init: more functions

--- includes/functions.php
+++ includes/functions.php
@@ -306,3 +306,98 @@ function wsklad_admin_accounts_get_url($action = 'list', $account_id = '')
 
 	return admin_url($path);
 }
+
+/**
+ * Convert mysql datetime to PHP timestamp, forcing UTC. Wrapper for strtotime
+ *
+ * @param string $time_string Time string
+ * @param int|null $from_timestamp Timestamp to convert from
+ *
+ * @return int
+ */
+function wsklad_string_to_timestamp($time_string, $from_timestamp = null)
+{
+	$original_timezone = date_default_timezone_get();
+
+	date_default_timezone_set('UTC');
+
+	if(null === $from_timestamp)
+	{
+		$next_timestamp = strtotime($time_string);
+	}
+	else
+	{
+		$next_timestamp = strtotime($time_string, $from_timestamp);
+	}
+
+	date_default_timezone_set($original_timezone);
+
+	return $next_timestamp;
+}
+
+/**
+ * Helper to retrieve the timezone string for a site until
+ *
+ * @return string PHP timezone string for the site
+ */
+function wsklad_timezone_string()
+{
+	// If site timezone string exists, return it
+	$timezone = get_option('timezone_string');
+
+	if($timezone)
+	{
+		return $timezone;
+	}
+
+	// Get UTC offset, if it isn't set then return UTC
+	$utc_offset = (int) get_option('gmt_offset', 0);
+	if(0 === $utc_offset)
+	{
+		return 'UTC';
+	}
+
+	// Adjust UTC offset from hours to seconds
+	$utc_offset *= 3600;
+
+	// Attempt to guess the timezone string from the UTC offset
+	$timezone = timezone_name_from_abbr('', $utc_offset);
+	if($timezone)
+	{
+		return $timezone;
+	}
+
+	// Last try, guess timezone string manually
+	foreach(timezone_abbreviations_list() as $abbr)
+	{
+		foreach($abbr as $city)
+		{
+			// WordPress restrict the use of date(), since it's affected by timezone settings, but in this case is just what we need to guess the correct timezone
+			if((bool) date('I') === (bool) $city['dst'] && $city['timezone_id'] && (int) $city['offset'] === $utc_offset)
+			{
+				return $city['timezone_id'];
+			}
+		}
+	}
+
+	return 'UTC';
+}
+
+/**
+ * Get timezone offset in seconds
+ *
+ * @return float
+ */
+function wsklad_timezone_offset()
+{
+	$timezone = get_option('timezone_string');
+
+	if($timezone)
+	{
+		$timezone_object = new \DateTimeZone($timezone);
+
+		return $timezone_object->getOffset(new DateTime('now'));
+	}
+
+	return (float) get_option('gmt_offset', 0) * HOUR_IN_SECONDS;
+}
