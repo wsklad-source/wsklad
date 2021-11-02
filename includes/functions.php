5f14041fd7d55ddb471bf3826a73ef065b96d1bb
digiom-ceo <ceo@digiom.ru>
2021-11-02 17:45:38 +0300

    Init: more functions

--- includes/functions.php
+++ includes/functions.php
@@ -76,3 +76,83 @@ function wsklad_load_textdomain()
 	load_textdomain('wsklad', WP_LANG_DIR . '/plugins/wsklad-' . $locale . '.mo');
 	load_textdomain('wsklad', WSKLAD_PLUGIN_PATH . 'languages/wsklad-' . $locale . '.mo');
 }
+
+/**
+ * Is Wsklad admin request?
+ *
+ * @return bool
+ */
+function is_wsklad_admin_request()
+{
+	if(false !== is_admin() && 'wsklad' === wsklad_get_var($_GET['page'], ''))
+	{
+		return true;
+	}
+
+	return false;
+}
+
+/**
+ * Get data if set, otherwise return a default value or null
+ * Prevents notices when data is not set
+ *
+ * @param mixed $var variable
+ * @param string $default default value
+ *
+ * @return mixed
+ */
+function wsklad_get_var(&$var, $default = null)
+{
+	return isset($var) ? $var : $default;
+}
+
+/**
+ * Pretty debug
+ *
+ * @param $data
+ * @param bool $die
+ */
+function wsklad_debug($data, $die = true)
+{
+	echo "<pre>";
+	var_dump($data);
+	echo "</pre>";
+
+	if($die)
+	{
+		die;
+	}
+}
+
+/**
+ * Is Wsklad admin section request?
+ *
+ * @param string $section
+ *
+ * @return bool
+ */
+function is_wsklad_admin_section_request($section = '')
+{
+	if('' === $section)
+	{
+		return false;
+	}
+
+	if(is_wsklad_admin_request() && wsklad_get_var($_GET['section'], '') === $section)
+	{
+		return true;
+	}
+
+	return false;
+}
+
+/**
+ * Outputs a "back" link so admin screens can easily jump back a page
+ *
+ * @param string $label title of the page to return to.
+ * @param string $url URL of the page to return to.
+ */
+function wsklad_admin_back_link($label, $url)
+{
+	echo '<h2 style="margin-bottom: 20px;margin-top: 15px;">' . esc_attr($label) . '<small class="wc-admin-breadcrumb" style="margin-left: 10px;"><a href="' . esc_url($url) . '" aria-label="' . esc_attr($label) . '"> &#x2934;</a></small></h2>';
+}
