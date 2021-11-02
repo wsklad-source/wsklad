61f607904fd7908dacebd81d4a4be3ee4293c716
digiom-ceo <ceo@digiom.ru>
2021-11-02 16:13:58 +0300

    Init: core

---
+++ includes/Core.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Namespace
+ */
+namespace Wsklad;
+
+/**
+ * Only WordPress
+ */
+defined('ABSPATH') || exit;
+
+/**
+ * Dependencies
+ */
+use Wsklad\Traits\Singleton;
+use Wsklad\Admin;
+
+/**
+ * Class Core
+ *
+ * @package Wsklad
+ */
+class Core
+{
+	/**
+	 * Traits
+	 */
+	use Singleton;
+
+	/**
+	 * Core constructor
+	 */
+	public function __construct()
+	{
+		// hook
+		do_action('wsklad_before_loading');
+
+		// localization files
+		wsklad_load_textdomain();
+
+		// init
+		add_action('init', [$this, 'init'], 3);
+
+		// admin
+		if(false !== is_admin())
+		{
+			add_action('init', [Admin::class, 'instance'], 5);
+		}
+
+		// hook
+		do_action('wsklad_after_loading');
+	}
+
+	/**
+	 * Initialization
+	 */
+	public function init()
+	{
+		// hook
+		do_action('wsklad_before_init');
+
+
+
+		// hook
+		do_action('wsklad_after_init');
+	}
+}
