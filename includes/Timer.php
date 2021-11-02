8cab1e90b900eb27d4ed729726d91c15fccd72a1
digiom-ceo <ceo@digiom.ru>
2021-11-02 16:19:37 +0300

    Init: timer

---
+++ includes/Timer.php
@@ -0,0 +1,124 @@
+<?php
+/**
+ * Namespace
+ */
+namespace Wsklad;
+
+/**
+ * Only WordPress
+ */
+defined('ABSPATH') || exit;
+
+/**
+ * Class Timer
+ *
+ * @package Wsklad
+ */
+class Timer
+{
+	/**
+	 * Maximum time is available in sec
+	 *
+	 * @var int
+	 */
+	private $maximum = 30;
+
+	/**
+	 * Timer started
+	 *
+	 * @var int|float
+	 */
+	private $started = 0;
+
+	/**
+	 * Timer constructor.
+	 */
+	public function __construct()
+	{
+		if(isset($_SERVER['REQUEST_TIME_FLOAT']))
+		{
+			$this->setStarted($_SERVER['REQUEST_TIME_FLOAT']);
+		}
+	}
+
+	/**
+	 * Available maximum secs
+	 *
+	 * @return int
+	 */
+	public function getMaximum()
+	{
+		return $this->maximum;
+	}
+
+	/**
+	 * Set maximum available secs
+	 *
+	 * @param int $maximum
+	 */
+	public function setMaximum($maximum)
+	{
+		$this->maximum = $maximum;
+	}
+
+	/**
+	 * Timer started
+	 *
+	 * @return int
+	 */
+	public function getStarted()
+	{
+		return $this->started;
+	}
+
+	/**
+	 * Set timer started
+	 *
+	 * @param int $started
+	 */
+	public function setStarted($started)
+	{
+		$this->started = $started;
+	}
+
+	/**
+	 * Current executions time in secs
+	 *
+	 * @return float|int
+	 */
+	public function getExecutionSeconds()
+	{
+		return microtime(true) - $this->getStarted();
+	}
+
+	/**
+	 * Available secs for executions
+	 *
+	 * @return float|int
+	 */
+	public function getAvailableSeconds()
+	{
+		return $this->getMaximum() - $this->getExecutionSeconds();
+	}
+
+	/**
+	 * Get available seconds bigger than secs
+	 *
+	 * @param int $seconds
+	 *
+	 * @return bool
+	 */
+	public function isRemainingBiggerThan($seconds = 30)
+	{
+		$max_execution_time = $this->getMaximum();
+
+		if($max_execution_time === 0)
+		{
+			return true;
+		}
+
+		$remaining_seconds = $this->getAvailableSeconds();
+
+		return ($remaining_seconds >= $seconds);
+	}
+}
