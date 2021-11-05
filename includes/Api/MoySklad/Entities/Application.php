99072d105e036bef501843945e6b9e82e4e4f4fc
digiom-ceo <ceo@digiom.ru>
2021-11-05 20:38:25 +0300

    Init: Api - Entities - Application

---
+++ includes/Api/MoySklad/Entities/Application.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Namespace
+ */
+namespace Wsklad\Api\MoySklad\Entities;
+
+/**
+ * Only WordPress
+ */
+defined('ABSPATH') || exit;
+
+/**
+ * Class Application
+ *
+ * @package Wsklad\Api\MoySklad\Entities
+ */
+class Application extends MetaEntity
+{
+
+}
