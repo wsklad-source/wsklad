915190044692df754f366495c603da3d42e60a24
digiom-ceo <ceo@digiom.ru>
2021-11-05 15:39:34 +0300

    Fix: Api - Exceptions - ApiClientException

--- includes/Api/MoySklad/Exceptions/ApiClientException.php
+++ includes/Api/MoySklad/Exceptions/ApiClientException.php
@@ -21,28 +21,28 @@ use Exception;
  */
 class ApiClientException extends Exception
 {
-    /**
-     * @var int
-     */
+	/**
+	 * @var int
+	 */
 	protected $statusCode;
 
-    /**
-     * @var string
-     */
-    protected $reasonPhrase;
+	/**
+	 * @var string
+	 */
+	protected $reasonPhrase;
 
-    /**
-     * ApiClientException constructor.
-     *
-     * @param string $uri
-     * @param int $statusCode
-     * @param string $reasonPhrase
-     */
-    public function __construct($uri, $statusCode, $reasonPhrase)
-    {
-        parent::__construct($uri . ': ' . $statusCode.' ' . $reasonPhrase, $statusCode);
+	/**
+	 * ApiClientException constructor.
+	 *
+	 * @param string $uri
+	 * @param int $statusCode
+	 * @param string $reasonPhrase
+	 */
+	public function __construct($uri, $statusCode, $reasonPhrase)
+	{
+		parent::__construct($uri . ': ' . $statusCode.' ' . $reasonPhrase, $statusCode);
 
-        $this->statusCode = $statusCode;
-        $this->reasonPhrase = $reasonPhrase;
-    }
+		$this->statusCode = $statusCode;
+		$this->reasonPhrase = $reasonPhrase;
+	}
 }
