885f42125db4bfdc75048fb9bd4108cff6c6708b
digiom-ceo <ceo@digiom.ru>
2021-11-05 15:38:59 +0300

    Init: Api - Exceptions - ApiClientException

---
+++ includes/Api/MoySklad/Exceptions/ApiClientException.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Namespace
+ */
+namespace Wsklad\Api\MoySklad\Exceptions;
+
+/**
+ * Only WordPress
+ */
+defined('ABSPATH') || exit;
+
+/**
+ * Dependencies
+ */
+use Exception;
+
+/**
+ * Class ApiClientException
+ *
+ * @package Wsklad\Api\MoySklad\Exceptions
+ */
+class ApiClientException extends Exception
+{
+    /**
+     * @var int
+     */
+	protected $statusCode;
+
+    /**
+     * @var string
+     */
+    protected $reasonPhrase;
+
+    /**
+     * ApiClientException constructor.
+     *
+     * @param string $uri
+     * @param int $statusCode
+     * @param string $reasonPhrase
+     */
+    public function __construct($uri, $statusCode, $reasonPhrase)
+    {
+        parent::__construct($uri . ': ' . $statusCode.' ' . $reasonPhrase, $statusCode);
+
+        $this->statusCode = $statusCode;
+        $this->reasonPhrase = $reasonPhrase;
+    }
+}
