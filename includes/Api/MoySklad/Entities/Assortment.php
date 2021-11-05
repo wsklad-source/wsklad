e47f316702fa34c6499c46ae9508d92d05b9be5e
digiom-ceo <ceo@digiom.ru>
2021-11-05 20:57:40 +0300

    Init: Api - Entities - Assortment

---
+++ includes/Api/MoySklad/Entities/Assortment.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Namespace
+ */
+namespace Wsklad\Api\MoySklad\Entities;
+
+/**
+ * Only WordPress
+ */
+defined('ABSPATH') || exit;
+
+/**
+ * Class Assortment
+ * Абстрактный класс, объединяющий товары, услуги, серии, комплекты и модификации
+ *
+ * @package Wsklad\Api\MoySklad\Entities
+ */
+class Assortment extends MetaEntity
+{
+}
