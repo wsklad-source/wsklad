80b1bbc5ed2160e0fb710063ef1f460584a38fd5
digiom-ceo <ceo@digiom.ru>
2021-11-08 13:02:02 +0300

    Fix: Admin - ListsTable

--- includes/Admin/Accounts/ListsTable.php
+++ includes/Admin/Accounts/ListsTable.php
@@ -180,7 +180,7 @@ class ListsTable extends TableAbstract
 
 		if('deleted' === $item['status'])
 		{
-			unset($actions['update']);
+			unset($actions['update'], $actions['delete']);
 			$actions['remove'] = '<a href="' . wsklad_admin_accounts_get_url('remove', $item['account_id']) . '">' . __('Remove forever', 'wsklad') . '</a>';
 		}
 
