99ed3a2c8c511e69897ce82990e9ed09e0385b30
digiom-ceo <ceo@digiom.ru>
2021-11-09 19:37:25 +0300

    Init: Logger

---
+++ includes/Logger.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Namespace
+ */
+namespace Wsklad;
+
+/**
+ * Only WordPress
+ */
+defined('ABSPATH') || exit;
+
+/**
+ * Dependencies
+ */
+use \Monolog\Logger as Monolog;
+
+/**
+ * Class Logger
+ *
+ * @package Wsklad
+ */
+class Logger extends Monolog
+{
+}
