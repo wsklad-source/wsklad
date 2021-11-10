68ba3bb6da394c52b9e3ddc23cb6fd203ab48f5f
digiom-ceo <ceo@digiom.ru>
2021-11-10 13:52:13 +0300

    Fix: more

--- templates/accounts/create.php
+++ templates/accounts/create.php
@@ -1,13 +1,11 @@
 <?php defined('ABSPATH') || exit;?>
 
 <div class="row">
-    <div class="col pt-4 pb-3">
+    <div class="col pt-2 pb-2">
 		<?php _e('Use of the forms to connect your MoySklad account to WooCommerce.', 'wsklad'); ?>
     </div>
 </div>
 
-<div class="g-grid">
-    <div class="">
-        <?php do_action(WSKLAD_ADMIN_PREFIX . 'accounts_form_create_show'); ?>
-    </div>
+<div class="">
+    <?php do_action(WSKLAD_ADMIN_PREFIX . 'accounts_form_create_show'); ?>
 </div>
