abd5bb27ff7740b1f128ffc53759653f63b848da
digiom-ceo <ceo@digiom.ru>
2021-11-11 23:25:46 +0300

    Init: API - Moysklad - Clients - Endpoints - ApiEndpoint

---
+++ includes/Api/MoySklad/Clients/Endpoints/ApiEndpoint.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Namespace
+ */
+namespace Wsklad\Api\MoySklad\Clients\Endpoints;
+
+/**
+ * Only WordPress
+ */
+defined('ABSPATH') || exit;
+
+/**
+ * Interface ApiEndpoint
+ *
+ * Техническая аннотация, ставящаяся над конечным звеном запроса (например в <code>entity().counterparty().get()</code>
+ * метод <code>get()</code> должен быть отмечен этой аннотацией)
+ *
+ * @package Wsklad\Api\MoySklad\Clients\Endpoints
+ */
+interface ApiEndpoint
+{
+
+}
