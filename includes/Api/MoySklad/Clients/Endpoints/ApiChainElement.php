bebab6307f3af17ce05ef4c226fbb299d146deae
digiom-ceo <ceo@digiom.ru>
2021-11-11 23:25:16 +0300

    Init: API - Moysklad - Clients - Endpoints - ApiChainElement

---
+++ includes/Api/MoySklad/Clients/Endpoints/ApiChainElement.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Namespace
+ */
+namespace Wsklad\Api\MoySklad\Clients\Endpoints;
+
+/**
+ * Only WordPress
+ */
+defined('ABSPATH') || exit;
+
+/**
+ * Interface ApiChainElement
+ *
+ * Техническая аннотация, ставящаяся над промежуточным звеном запроса (например в <code>entity().counterparty().get()</code>
+ * метод <code>counterparty()</code> должен быть отмечен этой аннотацией)
+ *
+ * @package Wsklad\Api\MoySklad\Clients\Endpoints
+ */
+interface ApiChainElement
+{
+
+}
