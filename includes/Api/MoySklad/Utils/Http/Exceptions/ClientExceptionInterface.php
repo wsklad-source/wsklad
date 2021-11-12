c11ec1fbf89b6c94357c76f67de6ed6ed2118860
digiom-ceo <ceo@digiom.ru>
2021-11-12 14:34:30 +0300

    Init: API - MoySklad - Utils - Http - Exceptions

---
+++ includes/Api/MoySklad/Utils/Http/Exceptions/ClientExceptionInterface.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Namespace
+ */
+namespace Wsklad\Api\MoySklad\Utils\Http\Exceptions;
+
+/**
+ * Only WordPress
+ */
+defined('ABSPATH') || exit;
+
+/**
+ * Every HTTP client related exception MUST implement this interface.
+ */
+interface ClientExceptionInterface
+{
+}
---
+++ includes/Api/MoySklad/Utils/Http/Exceptions/RequestExceptionInterface.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Namespace
+ */
+namespace Wsklad\Api\MoySklad\Utils\Http\Exceptions;
+
+/**
+ * Only WordPress
+ */
+defined('ABSPATH') || exit;
+
+/**
+ * Dependencies
+ */
+use Psr\Http\Message\RequestInterface;
+
+/**
+ * Interface RequestExceptionInterface - Exception for when a request failed.
+ *
+ * Examples:
+ *      - Request is invalid (e.g. method is missing)
+ *      - Runtime request errors (e.g. the body stream is not seekable)
+ *
+ * @package Wsklad\Api\MoySklad\Utils\Http\Exceptions
+ */
+interface RequestExceptionInterface extends ClientExceptionInterface
+{
+    /**
+     * Returns the request.
+     *
+     * The request object MAY be a different object from the one passed to ClientInterface::sendRequest()
+     *
+     * @return RequestInterface
+     */
+    public function getRequest();
+}
