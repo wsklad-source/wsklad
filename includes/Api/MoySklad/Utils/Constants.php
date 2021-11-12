2148159fd5a86bb1b9356f7ebaa673c6f7e5e7b6
digiom-ceo <ceo@digiom.ru>
2021-11-12 09:04:27 +0300

    Init: API - Moysklad - Utils - Constants

---
+++ includes/Api/MoySklad/Utils/Constants.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Namespace
+ */
+namespace Wsklad\Api\MoySklad\Utils;
+
+/**
+ * Only WordPress
+ */
+defined('ABSPATH') || exit;
+
+/**
+ * Class Constants
+ *
+ * @package Wsklad\Api\MoySklad\Utils
+ */
+final class Constants
+{
+	const DATE_FORMAT_PATTERN = 'yyyy-MM-dd HH:mm:ss.SSS';
+    const API_PATH = '/api/remap/1.2';
+}
