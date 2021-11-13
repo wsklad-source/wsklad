70cbb6db38977b6bf264f605ff9a1f8554e384b6
digiom-ceo <ceo@digiom.ru>
2021-11-13 15:28:06 +0300

    Fix: more

--- vendor/wsklad/moysklad/src/Utils/Params/SearchParam.php
+++ vendor/wsklad/moysklad/src/Utils/Params/SearchParam.php
@@ -2,7 +2,7 @@
 /**
  * Namespace
  */
-namespace Wsklad\Api\MoySklad\Utils\Params;
+namespace Wsklad\MoySklad\Utils\Params;
 
 /**
  * Only WordPress
@@ -12,7 +12,7 @@ defined('ABSPATH') || exit;
 /**
  * Class SearchParam
  *
- * @package Wsklad\Api\MoySklad\Utils\Params
+ * @package Wsklad\MoySklad\Utils\Params
  */
 class SearchParam extends ApiParam
 {
