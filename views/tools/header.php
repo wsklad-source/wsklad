362c1ae217c5ccc646782e83d35925f2e02b6b08
Oleg <109225168+frescoref@users.noreply.github.com>
2023-02-22 21:23:19 +0300

    Fix: more

--- views/tools/header.php
+++ views/tools/header.php
@@ -2,7 +2,7 @@
 
 <div class="header bg-white rounded-bottom border-top border-light border-5 p-2 pb-3 fs-5">
 
-	<a href="<?php echo wsklad()->admin()->utilityAdminAccountsGetUrl('all'); ?>" class="wp-heading-inline text-decoration-none"><?php _e('Moy Sklad', 'wsklad'); ?></a>
+	<a href="<?php echo esc_url_raw(wsklad()->admin()->utilityAdminAccountsGetUrl('all')); ?>" class="wp-heading-inline text-decoration-none"><?php _e('Moy Sklad', 'wsklad'); ?></a>
 >
 	<span class="wp-heading-inline"><?php _e('Tools', 'wsklad'); ?></span>
 
--- views/tools/item.php
+++ views/tools/item.php
@@ -1,16 +1,16 @@
 <?php defined('ABSPATH') || exit; ?>
 
-<div class="mb-2 mt-2 w-100" style="border-radius: 0!important; border: 1px solid #f7f7f7;">
+<div class="bg-white section-border rounded-3 mb-2 mt-2 w-100">
     <div class="card-body p-3">
         <h2 class="card-title mt-0">
-            <?php echo $args['name']; ?>
+            <?php printf('%s', sanitize_text_field($args['name'])); ?>
         </h2>
         <p class="card-text">
-            <?php echo $args['description']; ?>
+            <?php printf('%s', wp_kses_post($args['description'])); ?>
         </p>
     </div>
-    <div class="card-footer bg-light p-3">
-       <a class="text-decoration-none button button-primary" href="<?php echo $args['url']; ?>">
+    <div class="card-footer rounded-3 bg-light p-3">
+       <a class="text-decoration-none button button-primary" href="<?php echo esc_url($args['url']); ?>">
 	       <?php _e('Open', 'wsklad'); ?>
        </a>
     </div>
