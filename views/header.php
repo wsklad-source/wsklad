52936a74a77e333c2e59d7dfd63fb9fd23e3505b
Oleg <109225168+frescoref@users.noreply.github.com>
2023-02-22 21:40:22 +0300

    Fix: more

--- views/header.php
+++ views/header.php
@@ -1,9 +1,6 @@
 <?php defined('ABSPATH') || exit; ?>
 
 <div class="header bg-white rounded-bottom border-top border-light border-5 p-2 pb-3 fs-5">
-
-    <a href="<?php echo wsklad()->admin()->utilityAdminAccountsGetUrl('all'); ?>" class="wp-heading-inline text-decoration-none"><?php _e('Moy Sklad', 'wsklad'); ?></a>
-
+    <a href="<?php echo esc_url_raw(wsklad()->admin()->utilityAdminAccountsGetUrl('all')); ?>" class="wp-heading-inline text-decoration-none"><?php _e('Moy Sklad', 'wsklad'); ?></a>
     <?php do_action('wsklad_admin_header_items_show'); ?>
-
 </div>
