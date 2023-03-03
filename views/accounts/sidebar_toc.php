91ce55ffa4b27a3f333fefbc3008b28de23f70a6
Oleg <109225168+frescoref@users.noreply.github.com>
2023-03-03 14:45:37 +0300

    Fix: more

--- views/accounts/sidebar_toc.php
+++ views/accounts/sidebar_toc.php
@@ -2,20 +2,20 @@
 
 <?php do_action('wsklad_admin_accounts_update_before_sidebar_toc_show'); ?>
 
-<div class="card wsklad-sidebar-toc mt-0 mb-2 p-0" style="max-width: 100%;">
+<div class="card wsklad-sidebar-toc mt-0 mb-2 p-0 w-100">
     <?php if(isset($args['header'])): ?>
     <div class="card-header p-2">
-        <?php echo $args['header']; ?>
+        <?php printf('%s', wp_kses_post($args['header'])); ?>
     </div>
     <?php endif; ?>
     <?php if(isset($args['body'])): ?>
     <div class="card-body p-0">
-	    <?php echo $args['body']; ?>
+        <?php printf('%s', wp_kses_post($args['body'])); ?>
     </div>
     <?php endif; ?>
     <?php if(isset($args['footer'])): ?>
     <div class="card-footer p-2">
-	    <?php echo $args['footer']; ?>
+        <?php printf('%s', wp_kses_post($args['footer'])); ?>
     </div>
 	<?php endif; ?>
 </div>
