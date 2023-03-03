65b3a92d69dc552d635c4939ede486a20e7fca14
Oleg <109225168+frescoref@users.noreply.github.com>
2023-03-03 14:47:35 +0300

    Fix: more

--- views/accounts/sidebar_item.php
+++ views/accounts/sidebar_item.php
@@ -2,20 +2,20 @@
 
 <?php do_action('wsklad_admin_accounts_update_before_sidebar_item_show'); ?>
 
-<div class="card rounded-0 mb-2 mt-0 p-0 section-border" style="max-width: 100%; <?php if(isset($args['css'])) echo $args['css']; ?>">
+<div class="card rounded-0 mb-2 mt-0 p-0 section-border w-100" style="<?php if(isset($args['css'])) echo esc_attr($args['css']); ?>">
     <?php if(isset($args['header'])): ?>
     <div class="card-header p-2 rounded-0">
-        <?php echo $args['header']; ?>
+        <?php printf('%s', wp_kses_post($args['header'])); ?>
     </div>
     <?php endif; ?>
     <?php if(isset($args['body'])): ?>
     <div class="card-body p-0">
-	    <?php echo $args['body']; ?>
+        <?php printf('%s', wp_kses_post($args['body'])); ?>
     </div>
     <?php endif; ?>
     <?php if(isset($args['footer'])): ?>
     <div class="card-footer p-2">
-	    <?php echo $args['footer']; ?>
+        <?php printf('%s', wp_kses_post($args['footer'])); ?>
     </div>
 	<?php endif; ?>
 </div>
