f707daf83efcb500c6a880dd82811d4af962b388
Oleg <109225168+frescoref@users.noreply.github.com>
2023-03-11 10:46:28 +0300

    Prepare: 0.3.4

--- wsklad.php
+++ wsklad.php
@@ -3,7 +3,7 @@
  * Plugin Name: WSKLAD
  * Plugin URI: https://wordpress.org/plugins/wsklad
  * Description: Implementation of a mechanism for flexible exchange of various data between Moy Sklad and a site running WordPress.
- * Version: 0.3.3
+ * Version: 0.3.4
  * Requires at least: 5.2
  * Requires PHP: 7.0
  * Text Domain: wsklad
