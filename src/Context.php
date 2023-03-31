a12132b86daf90e8e2be2b9f0b1d36d7e41d3204
Oleg <109225168+frescoref@users.noreply.github.com>
2023-04-01 00:54:33 +0300

    Fix: more

--- src/Context.php
+++ src/Context.php
@@ -9,4 +9,18 @@ defined('ABSPATH') || exit;
  */
 final class Context extends \Digiom\Woplucore\Context
 {
+	/**
+	 * @return bool
+	 */
+	public function isPluginAdmin(): bool
+	{
+		if(!isset($_GET['page']) || false === $this->isAdmin())
+		{
+			return false;
+		}
+
+		$pos = strrpos(sanitize_key($_GET['page']), $this->getSlug());
+
+		return $pos !== false;
+	}
 }
